8bc2f58834e53203f7a91f606c09a1daaede704e
nadakichi <shogo.oilt@gmail.com>
2022-04-21 23:12:35 +0900

    クレジットカード決済機能

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,13 @@
 Rails.application.routes.draw do
+  get 'cards/new'
+  get 'users/show'
   devise_for :users
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root 'items#index'
+
+  resources :users, only: [:show, :update]
+  resources :cards, only: [:new, :create]
+  resources :items, only: :order do
+    post 'order', on: :member
+  end
 end
+
